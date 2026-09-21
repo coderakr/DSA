956d69a335f5cca55ecae5e691f41cb40ad721bb
coderakr <139627558+coderakr@users.noreply.github.com>
2026-09-21 09:01:22 +0530

    add min heap stl

--- Heap/minHeapStl.cpp
+++ Heap/minHeapStl.cpp
@@ -5,12 +5,16 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    priority_queue<int> pq;
+    priority_queue<int, vector<int>, greater<int>> pq;
     pq.push(1);
     pq.push(2);
     pq.push(8);
+    pq.push(-1);
     pq.push(3);
     pq.push(4);
-    cout << pq.top();
+    cout << pq.top() << endl;    // -1
+    pq.pop();
+    pq.pop();
+    cout << pq.top() << endl;    // 2
     return 0;
 }
